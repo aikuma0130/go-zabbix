564d159a904403a8641b1f24035cc24ac36d712c
Dakie <ktaka0130@gmail.com>
2018-12-02 20:37:02 +0900

    Add Login()

--- cmds/zbx/main.go
+++ cmds/zbx/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/aikuma0130/go-zabbix"
 )
 
 func main() {
-	zabbix.Client.URL = "http://localhost/zabbix"
-	zabbix.Client.Login()
+	zabbix.Client.URL = "http://localhost:8081"
+	zabbix.Client.User = "Admin"
+	zabbix.Client.Password = "zabbix"
+	err := zabbix.Client.Login()
+	if err != nil {
+		return
+	}
+	fmt.Printf("%v", zabbix.Client.Auth)
 }
--- query.go
+++ query.go
@@ -1,27 +1,61 @@
 package zabbix
 
 import (
-	"fmt"
-	"net/url"
+	"bytes"
+	"encoding/json"
+	"net/http"
 )
 
 type Zabbix struct {
-	URL string
+	URL      string
+	User     string
+	Password string
+	Auth     string
 }
 
 type ZabbixResponse struct {
-	Result map[string]interface{} `json:result`
+	Result interface{} `json:result`
+}
+
+type ZabbixRequest struct {
+	Jsonrpc string      `json:"jsonrpc"`
+	Method  string      `json:"method"`
+	Params  interface{} `json:"params"`
+	ID      int         `json:"id"`
+	Auth    string      `json:"auth,omitempty"`
 }
 
 var Client Zabbix
 
-func (zabbix Zabbix) Login() (ZabbixResponse, error) {
-	vals := make(url.Values)
-	fmt.Println(zabbix.URL)
-	var res ZabbixResponse
-	res.Result = map[string]interface{}{
-		"result": "ok",
+func (zabbix *Zabbix) Login() error {
+	u := zabbix.URL + "/api_jsonrpc.php"
+	data := ZabbixRequest{}
+	data.Jsonrpc = "2.0"
+	data.Method = "user.login"
+	data.ID = 1
+	//data.Auth = ""
+	data.Params = map[string]interface{}{
+		"user":     zabbix.User,
+		"password": zabbix.Password,
 	}
 
-	return res, nil
+	jsonStr, _ := json.Marshal(&data)
+
+	req, _ := http.NewRequest("POST", u, bytes.NewBuffer(jsonStr))
+	req.Header.Set("Content-Type", "application/json-rpc")
+	client := new(http.Client)
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+
+	var response ZabbixResponse
+	if err := json.NewDecoder(res.Body).Decode(&response); err != nil {
+		return err
+	}
+
+	zabbix.Auth = response.Result.(string)
+
+	return nil
 }
